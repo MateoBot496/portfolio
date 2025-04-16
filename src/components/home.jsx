e8c3d70f3f970dcd5bc740a1120dabbc4af339ef
MateoBot496 <mateomontesacosta@gmail.com>
2025-04-16 13:19:32 +0200

    Mejorar rendimiento

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -3,7 +3,7 @@ export default function Home(){
         <>
         <div className="home column center " id="home">
             
-            <div className="row" style={{width: "100%"}}>
+            <div className="row homeTextDiv" style={{width: "100%"}}>
                 <div className="homeText left">Si nos define una palabra es ...</div>
                 
                 <div className="homeText">Bienvenido a mi portafolio.</div>
